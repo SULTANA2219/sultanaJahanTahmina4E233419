2c68364ede38010c49d4d7d5d73ca6d94923529f
Sultana Jahan Tahmina <hscsultana22@gmail.com>
2025-11-07 20:20:46 +0600

    Implement basic output of numbers from 1 to N

---
+++ pA2.cpp
@@ -0,0 +1,24 @@
+/*
+* Course: Competitive Programming Sessional (CCE-2310)
+* Assignment: Codeforces ICPC Assiut University Community
+* Problem ID: A
+* Author: Sultana Jahan Tahmina (ID:E243419)
+* Language/Version: GNU G++20 13.2 (64 bit, winlibs)
+* Codeforces Profile: https://codeforces.com/profile/hscsultana22
+* Date: 2025-11-07
+* Statement: 
+https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/A
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cout << i << endl;
+    }
+    return 0;
+}
