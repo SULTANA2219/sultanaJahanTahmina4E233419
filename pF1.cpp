53181c4a987ae2f0a7cad2e6fc1d9c80748f5cf7
Sultana Jahan Tahmina <hscsultana22@gmail.com>
2025-11-07 20:15:56 +0600

    Create pF1.cpp

---
+++ pF1.cpp
@@ -0,0 +1,24 @@
+/*
+* Course: Competitive Programming Sessional (CCE-2310)
+* Assignment: Codeforces ICPC Assiut University Community
+* Problem ID: F
+* Author: Sultana Jahan Tahmina (ID:E243419)
+* Language/Version: GNU G++20 13.2 (64 bit, winlibs)
+* Codeforces Profile: https://codeforces.com/profile/hscsultana22
+* Date: 2025-11-07
+* Statement: 
+https://codeforces.com/group/MWSDmqGsZm/contest/326175/problem/F
+*/
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+    cout << (A ^ B) << endl;
+    return 0;
+}
+
